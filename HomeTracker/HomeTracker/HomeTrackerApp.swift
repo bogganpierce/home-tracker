6ae920c4dc6a699e9db20e769db3003b38729930
Pierce Boggan <piboggan@microsoft.com>
2025-05-08 11:58:57 -0600

    Add initial project files and structure for HomeTracker app

---
+++ HomeTracker/HomeTracker/HomeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTrackerApp.swift
+//  HomeTracker
+//
+//  Created by Pierce Boggan on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
